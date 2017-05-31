730df97d74a0c8d63e6f81e3cdd3758d15991507
TommyX12 <tommyx058@gmail.com>
2017-05-31 16:14:08 -0400

    Setup basic test for Jurassic

---
+++ Assets/JurassicTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JurassicTest : MonoBehaviour {
+	
+	public static string test = @"
+		function f(n) {
+			if (n <= 0) return 1; 
+			return n * f(n - 1);
+		} 
+		
+		function Poisson(x, l) {
+			return Math.exp(-l) * Math.pow(l, x) / f(x);
+		}
+	
+		[Poisson(4, 5), Poisson(5, 5)].toString();
+	";
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("hello?");
+		var engine = new Jurassic.ScriptEngine();
+		Debug.Log(engine.Evaluate(test));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
