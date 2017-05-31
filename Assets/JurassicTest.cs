dd19b01f50570777fd49dcb09096444da8dca478
TommyX12 <tommyx058@gmail.com>
2017-05-31 16:22:04 -0400

    Tested calling c# method from js

--- Assets/JurassicTest.cs
+++ Assets/JurassicTest.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class JurassicTest : MonoBehaviour {
@@ -13,13 +14,20 @@ public class JurassicTest : MonoBehaviour {
 			return Math.exp(-l) * Math.pow(l, x) / f(x);
 		}
 	
-		[Poisson(4, 5), Poisson(5, 5)].toString();
+		// [Poisson(4, 5), Poisson(5, 5)].toString();
+		[fibonacci(0), fibonacci(1), fibonacci(2), fibonacci(3), fibonacci(4), fibonacci(5)].toString();
 	";
+	
+	int fibonacci(int n) {
+		if (n <= 1) return n;
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log("hello?");
 		var engine = new Jurassic.ScriptEngine();
+		engine.SetGlobalFunction("fibonacci", new Func<int, int>(fibonacci));
 		Debug.Log(engine.Evaluate(test));
 	}
 	
