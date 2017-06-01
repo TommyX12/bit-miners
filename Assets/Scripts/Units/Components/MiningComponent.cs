845f1f4ed96a2f43fc82c161d62daf8067e944ed
FreakingBarbarians <Paarthurnax@live.ca>
2017-06-01 17:37:11 -0400

    Added Mining Component
    - Mining Component

--- Assets/Scripts/Units/Components/MiningComponent.cs
+++ Assets/Scripts/Units/Components/MiningComponent.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MiningComponent : UnitComponent {
-    public float miningRange = 0.25f;
-    public float timeMultiplier = 1f;
+    public float MiningRange = 0.25f;
+    public float TimeMultiplier = 1f;
+    public int MaxCapacity = 30;
+    int currentCapacity = 0;
     float miningTimer;
     bool mining;
-    GameObject resource;
+    Resource resource;
     Collider2D miningCollider;
 
     public void startMining() {
@@ -15,6 +17,38 @@ public class MiningComponent : UnitComponent {
             return;
         }
 
-       // miningCollider.Cast(Vector2.zero)
+        if (currentCapacity >= MaxCapacity) {
+            return;
+        }
+
+        RaycastHit2D[] hitsBuffer = new RaycastHit2D[25];
+        int hitCount = miningCollider.Cast(Vector2.zero, hitsBuffer);
+        for (int i = 0; i < hitCount; i++) {
+            if (hitsBuffer[i].collider.gameObject.CompareTag("Resource")) {
+                resource = hitsBuffer[i].collider.gameObject.GetComponent<Resource>();
+                mining = true;
+                miningTimer = resource.CollectionTime * TimeMultiplier;
+                return;
+            }
+        }
+    }
+
+    public override void PausingFixedUpdate()
+    {
+        if (mining) {
+            miningTimer -= Time.fixedDeltaTime;
+            if (miningTimer <= 0) {
+                mining = false;
+                currentCapacity += resource.GetResource();
+                if (currentCapacity >= MaxCapacity) {
+                    currentCapacity = MaxCapacity;
+                }
+
+                if (((Vector2)(transform.position - resource.gameObject.transform.position)).magnitude > MiningRange) {
+                    mining = false;
+                    resource = null;
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -22,15 +22,6 @@ public class Unit : MyMono {
 
     public List<UnitComponent> components;
 
-    // Use this for initialization
-
-    void Start() {
-    }
-
-    // Update is called once per frame
-    void Update() {
-    }
-
     public UnitComponent getUnitcomponentOfType<T>() {
         foreach (UnitComponent comp in components) {
             if (comp.GetType() == typeof(T)) {
