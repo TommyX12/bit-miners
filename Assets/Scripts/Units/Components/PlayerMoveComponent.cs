fa86bdcf957da8e655a095918e1b5891052127ff
FreakingBarbarians <Paarthurnax@live.ca>
2017-06-01 21:43:51 -0400

    Added Player Mining Ability
    - Player can now mine by holding E near a resource
    - Player can now move around with WASD
    - Cleaned up some files
    - Created some prefabs

---
+++ Assets/Scripts/Units/Components/PlayerMoveComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveComponent : UnitComponent {
+
+    public float speed;
+
+    public override void PausingFixedUpdate()
+    {
+        base.PausingFixedUpdate();
+        int dx = 0;
+        int dy = 0;
+        if (Input.GetKey(KeyCode.W)) {
+            dy++;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            dy--;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            dx--;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dx++;
+        }
+
+        transform.position += new Vector3(dx, dy) * Time.fixedDeltaTime; 
+    }
+}
