9a5806358fd829777e82a739906bf53b9ad74d77
FreakingBarbarians <Paarthurnax@live.ca>
2017-06-03 19:05:38 -0400

    Added input text field that is scrollable

---
+++ Assets/InputCaratFix.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCaratFix : MonoBehaviour {
+
+    public GameObject contentInputCaret;
+    public GameObject Content;
+
+    bool jobDone = false;
+	
+	// Update is called once per frame
+	void Update () {
+        if (jobDone) {
+            return;
+        }
+
+        if (GameObject.Find("Content Input Caret") != null) {
+            contentInputCaret = GameObject.Find("Content Input Caret");
+            contentInputCaret.transform.SetParent(Content.transform);
+            jobDone = true;
+        }	
+	}
+}
