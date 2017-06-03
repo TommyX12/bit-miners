9b4f6f377331c11f0ca9098866a8a80b70925f73
TommyX12 <tommyx058@gmail.com>
2017-06-03 19:13:55 -0400

    Added ScriptEditor stub

---
+++ Assets/Scripts/ScriptSystem/ScriptEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptEditor : MyMono {
+	
+	public ScriptSystem CurrentScriptSystem {
+		get; private set;
+	}
+	
+	void Awake() {
+		
+	}
+
+	void Start() {
+		
+	}
+	
+	public void StartEdit(ScriptSystem scriptSystem) {
+		this.gameObject.SetActive(true);
+		MyMono.Paused = true;
+		
+		this.CurrentScriptSystem = scriptSystem;
+	}
+	
+	public void EndEdit() {
+		this.gameObject.SetActive(false);
+		MyMono.Paused = false;
+		
+		this.CurrentScriptSystem = null;
+	}
+	
+	public override void NormalUpdate() {
+		
+	}
+	
+}
