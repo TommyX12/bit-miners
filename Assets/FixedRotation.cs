5e6dafd8e83b56d99d4f551056aa3d55598f67e3
FreakingBarbarians <Paarthurnax@live.ca>
2017-06-06 00:26:59 -0400

    Fixed Programming button rotating with drone

---
+++ Assets/FixedRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedRotation : MonoBehaviour {
+
+    private Quaternion rot;
+
+	// Use this for initialization
+	void Start () {
+        rot = gameObject.transform.rotation;
+	}
+
+    private void LateUpdate()
+    {
+        gameObject.transform.rotation = rot;
+    }
+}
