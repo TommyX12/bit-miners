3886cc0f6df6763ba64312d4711b8b4aef9a0180
TommyX12 <tommyx058@gmail.com>
2017-06-12 19:27:10 -0400

    Added ResourceManager

---
+++ Assets/Scripts/Utils/ResourceManager.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ResourceManager {
+	
+	private static Dictionary<string, Sprite> sprites;
+	
+	public static void Initialize() {
+		Sprite[] spriteObjects = Resources.LoadAll<Sprite>("Sprites");
+		sprites = new Dictionary<string, Sprite>();
+		foreach (Sprite sprite in spriteObjects) {
+			sprites[sprite.name] = sprite;
+			Debug.Log(sprite.name);
+		}
+	}
+	
+	public static Sprite GetSprite(string name) {
+		Sprite result;
+		sprites.TryGetValue(name, out result);
+		return result;
+	}
+	
+}
