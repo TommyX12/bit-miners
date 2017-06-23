c6d4a80af58161414f3084a11ebc674e87bc3f6f
FreakingBarbarians <Paarthurnax@live.ca>
2017-06-23 15:25:50 -0400

    Created Simple Linear Dialogue System

---
+++ Assets/Dialogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+    
+    public string text;
+    public Dialogue next;
+    public List<string> conditions;
+
+    public virtual void PreDisplay() {
+
+    }
+
+    public virtual void PostDisplay()
+    {
+
+    }
+
+    public void Run(){
+        PreDisplay();
+        PostDisplay();
+    }
+
+}
---
+++ Assets/TutorialSystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialSystem : MyMono {
+
+    public Dictionary<string, bool> conditions;
+    public DialogueDisplay display;
+
+    public Dialogue current;
+    public Dialogue start;
+
+    public void next() {
+        if (check(current.conditions))
+        {
+            current = current.next;
+            current.Run();
+            display.Show(current.text);
+        }
+        else{
+            display.Show(current.text);
+        }
+    }
+
+    public bool check(List<string> bools){
+        foreach (string s in bools) {
+            if (conditions.ContainsKey(s))
+            {
+                if (conditions[s])
+                {
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public override void PausingFixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            next();
+        }
+    }
+}
