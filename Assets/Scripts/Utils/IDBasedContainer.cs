3a2426149c44547b43e30e45ef2ed72826fb1a6f
TommyX12 <tommyx058@gmail.com>
2017-07-10 00:27:49 -0400

    Added abstract ID based container

---
+++ Assets/Scripts/Utils/IDBasedContainer.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+public class IDBasedContainer<T> 
+    where T:IIDBasedElememt
+{
+    private Dictionary<int, T> data = new Dictionary<int, T>();
+    private Stack<int> unusedID = new Stack<int>();
+    public int MaxID {
+        get; private set;
+    }
+    
+    public IDBasedContainer() {
+        this.MaxID    = -1;
+    }
+    
+    public int Add(T element) {
+        int id = 0;
+        if (this.unusedID.Count == 0) {
+            id = this.MaxID + 1;
+        }
+        else {
+            id = this.unusedID.Pop();
+        }
+        this.AddWithID(id, element);
+        return id;
+    }
+    
+    public T Get(int id) {
+        return this.data[id];
+    }
+    
+    public void Remove(T element) {
+        this.Remove(element.ID);
+    }
+    
+    public void Remove(int id) {
+        this.data.Remove(id);
+        this.unusedID.Push(id);
+    }
+    
+    public void AddWithID(int id, T element) {
+        if (id < 0) {
+            this.Add(element);
+        }
+        else {
+            this.data[id] = element;
+            element.ID = id;
+            this.MaxID = Math.Max(id, this.MaxID);
+        }
+    }
+    
+    public void BuildUnusedID() {
+        this.unusedID.Clear();
+        for (int i = 0; i <= this.MaxID; ++i) {
+            if (!this.data.ContainsKey(i)) {
+                this.unusedID.Push(i);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/IIDBasedElement.cs
@@ -0,0 +1,5 @@
+public interface IIDBasedElememt {
+    int ID {
+        get; set;
+    }
+}
