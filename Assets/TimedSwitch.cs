6309a44f77fcbadb3183fb49f60f8c2a87ec5b0d
FreakingBarbarians <Paarthurnax@live.ca>
2017-07-13 16:17:27 -0400

    Set Up Levels 3,4,5

---
+++ Assets/TimedSwitch.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class TimedSwitch : MyMono {
+
+    public Sprite[] onSprite;
+    public Sprite offSprite;
+    public float maxtime;
+    public SpriteRenderer rendy;
+    public bool on;
+    public BoxCollider2D boxy;
+
+    float selector;
+
+
+    private void Start()
+    {
+        rendy = GetComponent<SpriteRenderer>();
+        boxy = GetComponent<BoxCollider2D>();
+    }
+
+    public override void PausingFixedUpdate()
+    {
+        base.PausingFixedUpdate();
+
+        if (on == false)
+        {
+            rendy.sprite = offSprite;
+        }
+        else {
+            selector -= Time.fixedDeltaTime;
+            if (selector <= 0)
+            {
+                on = false;
+            }
+            else {
+                rendy.sprite = onSprite[(int)((selector / maxtime) * onSprite.Length-1)];
+            }
+        }
+    }
+
+    public void SetOn() {
+        selector = maxtime;
+        on = true;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        SetOn();
+    }
+
+}
